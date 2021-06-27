8382f6e10129c2d1fb4188b03a56d07dfd1905bc
Petteri <petteri.malkavaara1@gmail.com>
2021-06-27 19:24:15 +0300

    Ignore xp on silk touch

--- src/main/java/autoinv/Main.java
+++ src/main/java/autoinv/Main.java
@@ -18,6 +18,8 @@ import java.util.concurrent.ThreadLocalRandom;
 
 public class Main extends PluginBase implements Listener {
 
+    private static final boolean BLOCKS = true;
+
     public void onEnable() {
         this.getServer().getPluginManager().registerEvents(this, this);
     }
@@ -27,41 +29,45 @@ public class Main extends PluginBase implements Listener {
         if (e.isCancelled()) return;
         Player p = e.getPlayer();
         PlayerInventory inv = p.getInventory();
-        Item[] itemsToAdd = e.getDrops();
-        int count = itemsToAdd.length;
-        if (count > 0) {
-            List<Item> notAdded = new ArrayList<>();
-            for (int i = 0; i < count; i++) {
-                if (inv.canAddItem(itemsToAdd[i])) {
-                    inv.addItem(itemsToAdd[i]);
-                } else {
-                    notAdded.add(itemsToAdd[i]);
+        if (BLOCKS) {
+            Item[] itemsToAdd = e.getDrops();
+            int count = itemsToAdd.length;
+            if (count > 0) {
+                List<Item> notAdded = new ArrayList<>();
+                for (Item item : itemsToAdd) {
+                    if (inv.canAddItem(item)) {
+                        inv.addItem(item);
+                    } else {
+                        notAdded.add(item);
+                    }
                 }
+                e.setDrops(notAdded.toArray(new Item[0]));
             }
-            e.setDrops(notAdded.toArray(new Item[0]));
         }
         int xp = e.getDropExp();
         if (xp > 0) {
-            ArrayList<Integer> itemsWithMending = new ArrayList<>();
-            int size = inv.getSize();
-            for (int i = 0; i < 4; i++) {
-                if (inv.getArmorItem(i).getEnchantment(Enchantment.ID_MENDING) != null) {
-                    itemsWithMending.add(size + i);
+            if (e.getItem() != null && !(e.getBlock().canSilkTouch() && e.getItem().hasEnchantment(Enchantment.ID_SILK_TOUCH))) {
+                ArrayList<Integer> itemsWithMending = new ArrayList<>();
+                int size = inv.getSize();
+                for (int i = 0; i < 4; i++) {
+                    if (inv.getArmorItem(i).getEnchantment(Enchantment.ID_MENDING) != null) {
+                        itemsWithMending.add(size + i);
+                    }
                 }
-            }
-            if (itemsWithMending.isEmpty()) {
-                p.addExperience(xp);
-            } else {
-                Integer itemToRepair = itemsWithMending.get(ThreadLocalRandom.current().nextInt(itemsWithMending.size()));
-                Item toRepair = inv.getItem(itemToRepair);
-                if (toRepair instanceof ItemTool || toRepair instanceof ItemArmor) {
-                    if (toRepair.getDamage() > 0) {
-                        int dmg = toRepair.getDamage() - 2;
-                        if (dmg < 0) {
-                            dmg = 0;
+                if (itemsWithMending.isEmpty()) {
+                    p.addExperience(xp);
+                } else {
+                    Integer itemToRepair = itemsWithMending.get(ThreadLocalRandom.current().nextInt(itemsWithMending.size()));
+                    Item toRepair = inv.getItem(itemToRepair);
+                    if (toRepair instanceof ItemTool || toRepair instanceof ItemArmor) {
+                        if (toRepair.getDamage() > 0) {
+                            int dmg = toRepair.getDamage() - 2;
+                            if (dmg < 0) {
+                                dmg = 0;
+                            }
+                            toRepair.setDamage(dmg);
+                            inv.setItem(itemToRepair, toRepair);
                         }
-                        toRepair.setDamage(dmg);
-                        inv.setItem(itemToRepair, toRepair);
                     }
                 }
             }
